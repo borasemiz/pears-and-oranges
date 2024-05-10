e939dd635fcf508711e0a34cd616efba5533d12c
Bora Semiz <borasemiz50@gmail.com>
2024-05-10 16:56:55 +0200

    setup initial project and boilerplate

---
+++ src/app/page.tsx
@@ -0,0 +1,10 @@
+import { Text, Button } from '@radix-ui/themes';
+
+export default function Home() {
+  return (
+    <main>
+      <Text>Bora is</Text>
+      <Button>Awesome</Button>
+    </main>
+  );
+}
